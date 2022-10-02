0676f29ceb31c57529294be69976a93567e3839b
Adrian Leong <leongadrian36@gmail.com>
2022-10-02 22:05:52 +0800

    on press item animation

--- components/GoalItem.jsx
+++ components/GoalItem.jsx
@@ -2,23 +2,27 @@ import { Pressable, StyleSheet, Text, View } from "react-native";
 
 function GoalItem(props) {
 	return (
-		<Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-			<View style={styles.goalItem}>
+		<View style={styles.goalItem}>
+			<Pressable android_ripple={{color: '#dddddd'}} onPress={props.onDeleteItem.bind(this, props.id)}
+					   style={({pressed}) => pressed && styles.pressedItem}>
 				<Text style={styles.goalText}>{props.text}</Text>
-			</View>
-		</Pressable>
+			</Pressable>
+		</View>
 	)
 }
 
 const styles = StyleSheet.create({
 	goalItem: {
 		margin: 8,
-		padding: 8,
 		borderRadius: 6,
 		backgroundColor: "#5e0acc",
 	},
 	goalText: {
 		color: "white",
+		padding: 8,
+	},
+	pressedItem: {
+		opacity: 0.5,
 	},
 })
 
